9f9cbe64527cb2a596ced3f07de56de7ccc387e3
King Kenway <kingkenway16@gmail.com>
2020-09-06 10:44:06 +0100

    Removed heroku call

--- diary/settings.py
+++ diary/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku 
+# import django_heroku 
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -146,4 +146,4 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
